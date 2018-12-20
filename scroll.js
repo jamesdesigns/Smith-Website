13672eef733b00b9cc5f373891c26feb1b9e79ce
James Hooper <jameshooperdesigns@gmail.com>
2018-12-19 23:25:45 -0700

    edit content to the contact form

--- scroll.js
+++ scroll.js
@@ -1,26 +1,19 @@
-// NEW BACK TO TOP LINK
-// var backToTopBtn = document.getElementById("scrollUp");
-
-// backToTopBtn.onclick = function() {
-//     TweenLite.to(window, 1, {scrollTo:{y:"#section1", offsetY:90}});
-//   }
-
 
 /*Scroll to top when arrow up clicked BEGIN*/
-$(window).scroll(function() {
-    var height = $(window).scrollTop();
-    if (height > 100) {
-        $('#scrollUp').fadeIn();
-    } else {
-        $('#scrollUp').fadeOut();
-    }
-});
-$(document).ready(function() {
-    $("#scrollUp").click(function(event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
-    });
+// $(window).scroll(function() {
+//     var height = $(window).scrollTop();
+//     if (height > 100) {
+//         $('#scrollUp').fadeIn();
+//     } else {
+//         $('#scrollUp').fadeOut();
+//     }
+// });
+// $(document).ready(function() {
+//     $("#scrollUp").click(function(event) {
+//         event.preventDefault();
+//         $("html, body").animate({ scrollTop: 0 }, "slow");
+//         return false;
+//     });
 
-});
+// });
  /*Scroll to top when arrow up clicked END*/

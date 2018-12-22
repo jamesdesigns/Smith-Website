a5de62fb04f20b01295eb8e2f62ba7e48e89533e
James Hooper <jameshooperdesigns@gmail.com>
2018-12-21 17:14:39 -0700

    Add arrow button to the quote section in the Math page

--- quotes.js
+++ quotes.js
@@ -1,21 +1,22 @@
-(function() {
-    "use strict";
+var target = document.getElementById("mathQuote");
 
-    var el = document.getElementById("myQuote");
+var quotes = [
+    '<span class="customQuote">&#8220;</span>For mathematics is after all an anthropological phenomenon.<span class="customQuote">&#8221;</span>  <br><span style="margin-left: 25%"><em>&#8212;  Ludwig Wittgenstein, Remarks on the Foundations of Mathematics</em></span>',
+    '<span class="customQuote">&#8220;</span>By studying the process of geometric thought we may hope to arrive at what is most essential in the human mind.<span class="customQuote">&#8221;</span>  <br><span style="margin-left: 25%"><em>&#8212; Henry Poincare, Science and Method</em></span>'
+];
 
-    var quotes = [
-        ["Stay Hungry, Stay Foolish.", "Steve Jobs"],
-        ["Good Artists Copy, Great Artists Steal.", "Pablo Picasso"],
-        ["Argue with idoits, and you become an idiot.", "Oscar Wilde"]
-    ];
+function newQuote() {
+    var i = (Math.random() * quotes.length) | 0;
+    target.innerHTML = quotes[i];
+}
 
-    function rand(xs) {
-        return xs.slice(0).sort(function(){
-            return .5 - Math.random();
-        });
-    }
-    function quote(q) {
-        return "<p>&ldquo;" + q[0] +"&rdquo; &emdash; <em>" + q[1] +"</em></p>";
-    }
-    el.innerHTML = rand(myQuote).map(quote).join('');
-})
+newQuote();
+
+// var myArray = [
+//     '<span class="customQuote">&#8220;</span>For mathematics is after all an anthropological phenomenon.<span class="customQuote">&#8221;</span>  <br><span style="margin-left: 25%"><em>&#8212;  Ludwig Wittgenstein, Remarks on the Foundations of Mathematics</em></span>',
+//     '<span class="customQuote">&#8220;</span>By studying the process of geometric thought we may hope to arrive at what is most essential in the human mind.<span class="customQuote">&#8221;</span>  <br><span style="margin-left: 25%"><em>&#8212; Henry Poincare, Science and Method</em></span>'
+// ];
+
+// var randomQuote = myArray[Math.floor(Math.random()*myArray.length)];
+
+// document.createElement('blockquote').innerHTML = randomQuote;
